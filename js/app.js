453f1174637f4867ca2dcead57df7624d6d9c1d9
Arjun Nair <nair.arun47@yahoo.com>
2019-10-02 14:07:40 -0700

    Commit 2: most JS and HTML done

--- js/app.js
+++ js/app.js
@@ -6,54 +6,124 @@ let l = 10;
 
 
 class Tomagotchi {
-    constructor(name) {
-        this.name = name;
-        this.pokemon = [1, 2, 3, 4, 5]
-        this.age = ['infant', 'child', 'adult', 'elderly', 'dead'];
+    constructor(id) {
+        this.id = id;
+        this.age = i;
         this.hunger = j;
-        this.sleepiness = 10;
-        this.boredom = 10;
+        this.sleepiness = k;
+        this.boredom = l;
     }
-    aging() {
-        this.age[i]
-        for (i = 0; i <= age.length; i ++){
-            
-        }
-    }
+    // aging() {
+    //     this.age[i]
+    //     for (i = 0; i <= 4; i ++){
+    //         if (i=0){
+    //             console.log('Infant')
+    //         } else if (i=1){
+    //             console.log('Child')
+    //         } else if (i = 2) {
+    //             console.log('Adult')
+    //         } else if (i = 3) {
+    //             console.log('Elderly')
+    //         } else{
+    //             console.log('Dead')
+    //         }
+    //     }
+    // }
+    // aging()
 }
 
-// All Tomagotchi
-let t1 = new Tomagotchi();
+// All Tomagotchi ############################################
+let pokemon1 = new Tomagotchi(0); // Blaziken
+// let pokemon2 = new Tomagotchi(1); // Swampert
+// let pokemon3 = new Tomagotchi(2); // Sceptile
+// let pokemon4 = new Tomagotchi(3); // Gardevoir
+// let pokemon5 = new Tomagotchi(4); // Salamance
+// let pokemon6 = new Tomagotchi(5); // Metacross
+console.log(pokemon1)
+// console.log(pokemon2)
+// console.log(pokemon3)
+// console.log(pokemon4)
+// console.log(pokemon5)
+// console.log(pokemon6)
 
 // let test=(console.log(Tomagotchi.age[i]))
 
 document.getElementById("nameInput").placeholder = "Enter name";
 
-//game
+//game #######################################################
 const game = () => {
 
-    // Aging
-    // const aging = setInterval(function(){
-    //     Tomagotchi.age[i];
-    //     document.getElementById('age').innerText = i;
-    // }, 2000)
-    // // aging;
+    const age = setInterval(function () {
+        i++;
+        if (i === 10) {
 
+            clearInterval(age); // stops the interval
+            console.log('Dead')
+
+        }
+
+        document.getElementById('age').innerText = i;
+    }, 1000)
+    
+
+    // Feeding
+    const hungry = setInterval(function () {
+        j--;
+        if (j === 0) {
+
+            clearInterval(hungry); // stops the interval
+            console.log('interval stops')
+            
+        }
+        
+        document.getElementById('hunger').innerText = j;
+    }, 1000)
+
+    // Sleepiness
+    const sleepiness = setInterval(function () {
+        k--;
+        if (k === 0) {
+
+            clearInterval(sleepiness); // stops the interval
+            console.log('interval stops')
+
+        }
+
+        document.getElementById('sleepiness').innerText = k;
+    }, 1000)
+
+    // Boredom
+    const boredom = setInterval(function () {
+        l--;
+        if (l === 0) {
+
+            clearInterval(boredom); // stops the interval
+            console.log('interval stops')
+
+        }
+
+        document.getElementById('boredom').innerText = l;
+    }, 1000)
+    
+
+    // BUTTON
+    // const add = i ++;
+    // const feed = document.querySelector("#feed");
+    // feed.addEventListener('click', add)
+
+    //DIDSPLAY NAME
+
+    const values = document.querySelector('input');
+    const displays = document.querySelector('p');
+    function displayName() {
+        document.getElementById('display').innerText;
+    }
+
+    
+    values.addEventListener('click', displayName)
 
     
 
-
-    // Feeding
-    const $hunger = $('#hunger');
-
-    const hungry = setInterval(function () {
-        j--;
-        
-        document.getElementById('hunger').innerText = j;
-    }, 2000)
-
-    $hunger.text(`Hunger: ${this.hunger}`)
-
 }
 
-// game()
+game()
